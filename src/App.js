7726efe3b53cbaa33d583359f9a45a0156765f7b
shmoon2917 <shmoon2917@naver.com>
2020-08-31 14:37:40 +0900

    ch3. todo 앱 만들기 완료

--- src/App.js
+++ src/App.js
@@ -1,108 +1,29 @@
-import React, { useState } from 'react';
-import styled, { ThemeProvider } from 'styled-components';
-import Button from './components/ButtonWithSC';
-import Dialog from './components/Dialog';
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import TodoTemplate from './components/TodoTemplate';
+import TodoHead from './components/TodoHead';
+import TodoList from './components/TodoList';
+import TodoCreate from './components/TodoCreate';
+import { TodoProvider } from './TodoContext';
 
-const AppBlock = styled.div`
-  width: 512px;
-  margin: 0 auto;
-  margin-top: 4rem;
-  border: 1px solid black;
-  padding: 1rem;
-`;
-
-const ButtonGroup = styled.div`
-  & + & {
-    margin-top: 1rem;
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
   }
 `;
 
 function App() {
-  const [dialog, setDialog] = useState(false);
-  const onClick = () => {
-    setDialog(true);
-  };
-
-  const onConfirm = () => {
-    console.log('확인');
-    setDialog(false);
-  };
-
-  const onCancel = () => {
-    console.log('취소');
-    setDialog(false);
-  };
-
   return (
-    <ThemeProvider
-      theme={{
-        palette: {
-          blue: '#228be6',
-          gray: '#495057',
-          pink: '#f06595',
-        },
-      }}
-    >
-      <>
-        <AppBlock>
-          <ButtonGroup>
-            <Button size="large">Button</Button>
-            <Button>Button</Button>
-            <Button size="small">Button</Button>
-          </ButtonGroup>
-          <ButtonGroup>
-            <Button color="gray" size="large">
-              BUTTON
-            </Button>
-            <Button color="gray">BUTTON</Button>
-            <Button color="gray" size="small">
-              BUTTON
-            </Button>
-          </ButtonGroup>
-          <ButtonGroup>
-            <Button color="pink" size="large">
-              BUTTON
-            </Button>
-            <Button color="pink">BUTTON</Button>
-            <Button color="pink" size="small">
-              BUTTON
-            </Button>
-          </ButtonGroup>
-          <ButtonGroup>
-            <Button size="large" outline>
-              BUTTON
-            </Button>
-            <Button color="gray" outline>
-              BUTTON
-            </Button>
-            <Button color="pink" outline>
-              BUTTON
-            </Button>
-          </ButtonGroup>
-          <ButtonGroup>
-            <Button size="large" fullWidth>
-              BUTTON
-            </Button>
-            <Button size="large" color="gray" fullWidth>
-              BUTTON
-            </Button>
-            <Button color="pink" size="large" fullWidth onClick={onClick}>
-              삭제
-            </Button>
-          </ButtonGroup>
-        </AppBlock>
-        <Dialog
-          title="정말로 삭제하시겠습니까?"
-          confirmText="삭제"
-          cancelText="취소"
-          onConfirm={onConfirm}
-          onCancel={onCancel}
-          visible={dialog}
-        >
-          데이터를 정말로 삭제하시겠습니까?
-        </Dialog>
-      </>
-    </ThemeProvider>
+    <>
+      <TodoProvider>
+        <GlobalStyle />
+        <TodoTemplate>
+          <TodoHead />
+          <TodoList />
+          <TodoCreate />
+        </TodoTemplate>
+      </TodoProvider>
+    </>
   );
 }
 
--- src/ch2/App.test.js
+++ src/ch2/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from './ch2/App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
