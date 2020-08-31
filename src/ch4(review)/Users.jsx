2ad6f5be0e79ffaf22bdb356804050ec0bd28aff
shmoon2917 <shmoon2917@naver.com>
2020-08-31 22:03:59 +0900

    4-3. useAsync 커스텀 Hook 만들기 복습 완료

--- src/ch4(review)/Users.jsx
+++ src/ch4(review)/Users.jsx
@@ -1,44 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import useAsync from './useAsync';
+import User from './User';
+
+const getUsers = async () => {
+  const response = await axios.get(
+    'https://jsonplaceholder.typicode.com/users'
+  );
+  return response.data;
+};
 
 const Users = () => {
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [userId, setUserId] = useState(null);
+  const [state, refetch] = useAsync(getUsers, [], true);
+  const { loading, data: users, error } = state;
 
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
-  const fetchUsers = async () => {
-    try {
-      setUsers(null);
-      setError(null);
-      setLoading(true);
-      const { data } = await axios.get(
-        'https://jsonplaceholder.typicode.com/users'
-      );
-      setUsers(data);
-    } catch (e) {
-      setError(e);
-    }
-    setLoading(false);
+  const onClickUser = (id) => () => {
+    setUserId(id);
   };
 
   if (error) return <div>에러가 발생했습니다</div>;
   if (loading) return <div>로딩중...</div>;
-  if (!users) return <button onClick={fetchUsers}>불러오기</button>;
+  if (!users) return <button onClick={refetch}>불러오기</button>;
 
   return (
     <>
       <ul>
-        {users.map((user, id) => (
-          <li key={user.id}>
+        {users.map((user) => (
+          <li
+            key={user.id}
+            onClick={onClickUser(user.id)}
+            style={{ cursor: 'pointer' }}
+          >
             {user.username} ({user.name})
           </li>
         ))}
       </ul>
-      <button onClick={fetchUsers}>다시 불러오기</button>
+      <button onClick={refetch}>다시 불러오기</button>
+      {userId && <User id={userId} />}
     </>
   );
 };
